60cdbfcf5bf1b4dae2342aedd03810ab3eaf9bee
Elodie Cari <elodiecari@Elodies-MacBook-Pro.local>
2022-09-27 11:37:45 -0400

    Initial commit

---
+++ Instagram/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  Instagram
+//
+//  Created by Elodie Cari on 9/27/22.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
