1d51d95f736dd334b0505fbc452894e51b2ceb48
Elodie Cari <elodiecari@Elodies-MacBook-Pro.local>
2022-09-28 08:47:35 -0400

    Trying to debug tabbar

--- Instagram/Controller/MainTabController.swift
+++ Instagram/Controller/MainTabController.swift
@@ -15,18 +15,49 @@ class MainTabController: UITabBarController {
         super.viewDidLoad()
         configureViewControllers()
     }
-    
+
     // MARK: - Helpers
     
     func configureViewControllers() {
         view.backgroundColor = .white
-
-        let feed = FeedController()
-        let search = SearchController()
-        let image = ImageController()
-        let notification = NotificationController()
-        let profile = ProfileController()
+        let feed = templateNavigationController(
+            unselectedImage: UIImage(named: "home-selected") ?? UIImage(),
+            selectedImage: UIImage(named: "home-unselected") ?? UIImage(),
+            rootViewController: FeedController())
+        let search = templateNavigationController(
+            unselectedImage: UIImage(named: "search-selected") ?? UIImage(),
+            selectedImage: UIImage(named: "search-unselected") ?? UIImage(),
+            rootViewController: SearchController())
+        let image = templateNavigationController(
+            unselectedImage: UIImage(named: "plus-photo") ?? UIImage(),
+            selectedImage: UIImage(named: "plus-unselected") ?? UIImage(),
+            rootViewController: ImageController())
+        let notification = templateNavigationController(
+            unselectedImage: UIImage(named: "like-selected") ?? UIImage(),
+            selectedImage: UIImage(named: "like-unselected") ?? UIImage(),
+            rootViewController: NotificationController())
+        let profile = templateNavigationController(
+            unselectedImage: UIImage(named: "profile-selected") ?? UIImage(),
+            selectedImage: UIImage(named: "profile-unselected") ?? UIImage(),
+            rootViewController: ProfileController())
         
+//        let feed = FeedController()
+//        let search = SearchController()
+//        let image = ImageController()
+//        let notification = NotificationController()
+//        let profile = ProfileController()
         viewControllers = [feed, search, image, notification, profile]
+
+    }
+    
+    func templateNavigationController(
+        unselectedImage: UIImage,
+        selectedImage: UIImage,
+        rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+            nav.tabBarItem.image = unselectedImage
+            nav.tabBarItem.selectedImage = selectedImage
+            nav.navigationBar.tintColor = .black
+        return nav
     }
 }
