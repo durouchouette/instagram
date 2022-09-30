46f4df16ec0942e0d352aad53bbb975470379e5d
Elodie Cari <elodiecari@Elodies-MacBook-Pro.local>
2022-09-30 17:03:24 -0400

    User Profile UI

--- Instagram/View/Profile/ProfileCell.swift
+++ Instagram/View/Profile/ProfileCell.swift
@@ -5,4 +5,30 @@
 //  Created by Elodie Cari on 9/30/22.
 //
 
-import Foundation
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    private let postImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named:"venom-7")
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        return image
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .lightGray
+        addSubview(postImageView)
+        postImageView.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
